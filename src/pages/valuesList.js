cec45d60e09d57b75dadcfaa82f08d700053ea4b
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-22 11:30:19 -0500

    initial dataset

--- src/pages/valuesList.js
+++ src/pages/valuesList.js
@@ -1,5 +1,19 @@
-export const valuesList = [
-  { name: "family" },
-  { name: "love" },
-  { name: "adventure" },
-];
+export const valuesData = {
+  valuesList: {
+    "value-1": { id: "value-1", name: "family" },
+    "value-2": { id: "value-2", name: "love" },
+    "value-3": { id: "value-3", name: "adventure" },
+  },
+  columns: {
+    "all-values": {
+      id: "column-1",
+      title: "All Values",
+      valueIds: ["value-1", "value-2", "value-3"],
+    },
+    "chosen-values":{
+      id:"column-2",
+      title: "Chosen Values",
+      valueIds:[],
+    }
+  },
+};
