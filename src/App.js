606303fd28af1385748125f2f4dbbde59e787b77
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-28 14:57:11 -0500

    match file structure to course and still not working. New Approach

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import "./App.css";
 import React from "react";
-import ReactDOM from 'react-dom';
-import {Draggable, Droppable, DragDropContext} from 'react-beautiful-dnd';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "../src/pages/home";
-import Values from "../src/pages/values";
+import ReactDOM from "react-dom";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./data/initialData.jsx";
+import Column from "./column";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/values" component={Values} />
-        </Switch>
-      
-    </Router>
-  );
+class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+    // TODO: reorder our column
+  };
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
+  }
 }
 
 export default App;
