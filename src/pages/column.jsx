7663b394cd736a7e5f90eb5770e9cf66fd549126
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-28 13:33:05 -0500

    basic lists and tasks

--- src/pages/column.jsx
+++ src/pages/column.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import "../App.css";
 import { Droppable, Draggable } from "react-beautiful-dnd";
+import styled from 'styled-components';
+import Task from './task';
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 export default class Column extends React.Component {
   render() {
     return (
-      <Droppable droppableId={this.props.column.id}>
-        {provided => (
-          <ul
-            className="valuesList"
-            innerRef={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {this.props.values.map((value, index) => (
-              <Value key={value.id} value={value} index={index} />
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <TaskList>
+          {this.props.tasks.map(task => <Task key={task.id} task={task} />)}</TaskList>
+      </Container>
     );
   }
 }
