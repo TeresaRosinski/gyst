1b09de2502648bba28c541fa89842de212a1f532
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-30 11:23:26 -0500

    two sep columns

--- src/pages/newValues.jsx
+++ src/pages/newValues.jsx
@@ -15,41 +15,103 @@ const itemsFromBackend = [
 ];
 
 //mocked backend - will need to be editable - each group will be the id + key, will have tittle and list of items - items are dragged
-const columnsFromBackend = [
-  {
-    [uuid()]: {
-      name: "Values",
-      items: [itemsFromBackend],
-    },
+const columnsFromBackend = {
+  [uuid()]: {
+    name: "Values List",
+    items: itemsFromBackend,
   },
-];
+  [uuid()]: {
+    name: "Chosen Values",
+    items: [],
+  },
+};
 
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+  const column = columns[source.droppableId];
+  const copiedItems = [...column.items];
+  const [removed] = copiedItems.splice(source.index, 1);
+  copiedItems.splice(destination.index, 0, removed);
+  setColumns({
+    ...columns,
+    [source.droppableId]: {
+      ...column,
+      items: copiedItems,
+    },
+  });
+};
 function Values() {
   const [columns, setColumns] = useState(columnsFromBackend);
 
   return (
     <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext onDragEnd={(result) => console.log(result)}>
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
         {Object.entries(columns).map(([id, column]) => {
           return (
-            <Droppable droppableId={id}>
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? "lightblue"
-                        : "lightgrey",
-                      padding: 4,
-                      width: 250,
-                      minHeight: 500,
-                    }}
-                  ></div>
-                );
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
               }}
-            </Droppable>
+            >
+              <h2>{column.name}</h2>
+              <Droppable droppableId={id} key={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgrey",
+                        padding: 4,
+                        width: 250,
+                        minHeight: 500,
+                      }}
+                    >
+                      {column.items.map((item, index) => {
+                        return (
+                          <Draggable
+                            key={item.id}
+                            draggableId={item.id}
+                            index={index}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={{
+                                    userSelect: "none",
+                                    padding: 16,
+                                    margin: "0 0 8px 0",
+                                    minHeight: "50px",
+                                    backgroundColor: snapshot.isDragging
+                                      ? "#263B4A"
+                                      : "#456C86",
+                                    color: "white",
+                                    ...provided.draggableProps.style,
+                                  }}
+                                >
+                                  {item.content}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
           );
         })}
       </DragDropContext>
