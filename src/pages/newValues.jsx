f6f81eda724ee7261ed821d1238a7255bfa9b301
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-30 10:54:10 -0500

    fix uuid import

--- src/pages/newValues.jsx
+++ src/pages/newValues.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import {v4 as uuid} from "uuid"; 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "../App.css";
 import Card from "react-bootstrap/Card";
@@ -7,10 +8,26 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+
 function Values() {
+  
+  
+  //mocked backend - will need to be editable - each group will be the id + key, will have tittle and list of items - items are dragged
+  const columnsFromBackend =[
+    {
+      [uuid()]: {
+        name: 'Values', 
+        items:[]
+      }
+    }
+  ]
+
+
   return (
-    <div>
-      
+    <div style={{display: 'flex', justifyContent: 'center', height: '100%'  }}>
+      <DragDropContext onDragEnd={result => console.log(result)}>
+
+      </DragDropContext>
     </div>
   );
 }
