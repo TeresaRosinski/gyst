f85fcb71914cb3b9a47e684a328012be8b5b75be
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-30 10:35:00 -0500

    new attempt starting for values list. uuid add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import finalSpaceCharacters from "./data/valuesList.jsx";
 import "./App.css";
-import Values from './pages/values';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Values from "./pages/newValues.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-     <Values />
+      <Values />
     </div>
   );
 }
---
+++ src/pages/newValues.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "../App.css";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Values() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+export default Values;
